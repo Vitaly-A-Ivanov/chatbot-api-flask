ec612b42c6682ece9bbabd3b26f26c518364013c
Mark Rigden <mr632@kent.ac.uk>
2022-03-01 17:40:18 +0000

    Fix cross-origin for the Chatbot

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 app.config["UPLOAD_FOLDER"] = "pdf_files"
 
-CORS(app, resources={r"/upload/pdf": {"origins": "*"}})
+CORS(app, origins=["https://chat-bot-educ.herokuapp.com/*"])
 
 
 @app.route('/', methods=['GET'])
